df7fee12af6f24bb97503d0cc85ed338e4b6acfe
reecdev <237895043+reecdev@users.noreply.github.com>
2026-03-24 19:36:17 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import random, time
+
+tokens = []
+maps = {}
+
+def predict(word):
+    if word in maps:
+        try:
+            return random.choice(maps[word])
+        except:
+            return random.choice(tokens)
+    else:
+        return random.choice(tokens)
+    
+data = ""
+
+for word in data.split(" "):
+    tokens.append(word)
+
+for i, word in enumerate(tokens):
+    if not word in maps:
+        maps[word] = []
+    try:
+        maps[word].append(tokens[i + 1])
+    except:
+        continue
+
+# ------------------------------------------------------------------------------------------------
+
+prompt = "the"
+
+context = prompt.split(" ")
+print(prompt, end=" ")
+for i in range(10000):
+    prediction = predict(context[len(context)-1])
+    context.append(prediction)
+    print(prediction, end=" ", flush=True)
